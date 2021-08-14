5054df3039c2f8d74be33a26366bf00af7fc8d36
ralexrivero <ralexrivero@gmail.com>
2021-08-14 12:04:54 -0400

    try a prompt print $

---
+++ prompt.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ * @brief prints "$ ", wait for the user to enter a command, prints it on the next line.
+ * man 3 getline
+ * stop reading Keyword: “end-of-file”, or EOF (or Ctrl+D).
+ */
+
+int main(void)
+{
+}
