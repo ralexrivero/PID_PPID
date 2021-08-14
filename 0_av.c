9aaf58e6c8c5c270c542b856e13e7d82d78ad9a8
ralexrivero <ralexrivero@gmail.com>
2021-08-14 10:29:57 -0400

    printf argv without argc

--- 0_av.c
+++ 0_av.c
@@ -3,28 +3,19 @@
 #include <string.h>
 
 /**
- * @brief 
+ * @brief print all arguments without using argc
  * 
- * @param argv point to array of arguments given
- * @return int 
  */
-int main(__attribute__((unused))int argc, char *argv[])
+
+int main(int argc, char *argv[])
 {
-        int i = 0, j = 0;
-        char *varChr_str2 = NULL, *varChr_str3 = NULL;
 
-        varChr_str2 = strdup(argv);
-        varChr_str3 = strtok(varChr_str2, " ");
-        while (varChr_str3 != NULL)
+        char **ptrChr_str = NULL;
+
+        for (ptrChr_str = argv; *ptrChr_str != NULL; ptrChr_str++)
         {
-                varChr_str3 = strtok(NULL, " ");
-                printf("%d\n", i);
-                printf("%s\n", argv[i]);
-                i++;
-                
+                printf("%s\n", *ptrChr_str);
         }
-        
-
-        
+  
         return (0);
 }
