d70c62d5564b104f505c22a99053e866184b5edd
ralexrivero <ralexrivero@gmail.com>
2021-08-16 10:15:07 -0400

    comand line to string

--- 2.command_line_to_av.c
+++ 2.command_line_to_av.c
@@ -1,6 +1,32 @@
 #include "shell.h"
 
-int main(int argc, char **argv)
+int main(void)
 {
+        char str[] = "hello i am   introducing some text here";
+        char delim [] = " ";
+        char* piece;
+        char** tokens;
+        int i = 0;
+
         
+        printf("\33[1;36mBefore tokenize: \33[0m\"%s\"\n", str);
+        printf("\33[1;36mThe tokens:\33[0m \n");
+        piece = strtok(str, delim);
+        tokens[i] = malloc(sizeof(piece));
+
+        while (piece != NULL)
+        {
+        tokens[i] = malloc(sizeof(piece));
+        strcpy(tokens[i], piece);
+        printf("\33[0;33m%s\33[0m\n", tokens[i]);
+        printf("%s\n", piece);
+        piece = strtok(NULL, delim);
+        i++;
+        }
+        printf("\33[1;36mAfter tokenize: \33[0m\"%s\"\n", str);
+
+
+        for (i = 0; i < strlen(tokens); i++)
+        printf("\33[0;32mThe tokens: %s\33[0m\n", tokens[i]);
+        return (0);
 }
