3821f3d1c8dfa1d0f1bfdac58c55da685eb7822c
ralexrivero <ralexrivero@gmail.com>
2021-08-16 23:22:13 -0400

    execve fix

--- fork_wait_execve.c
+++ fork_wait_execve.c
@@ -2,10 +2,14 @@
 
 int main(void)
 {
+/* fork declaration */
         pid_t child_pid;
+        int status;
         int i = 0;
-        
-        
+/* execve declaration */
+        char cmd[] = "/usr/bin/ls";
+        char * argv[] = {"ls", "-l", "/tmp", NULL};
+        char * argenv[] = {NULL};
 
         if (child_pid == -1)
         {
@@ -17,9 +21,13 @@ int main(void)
                 child_pid = fork();
                 if (child_pid == 0)
                 {
-                        execv("/bin/ls", "-l /tmp");
-                        return (0);
+                        if (execve(cmd, argv, argenv) == -1)
+                        {
+                                perror("Could not execute execve");
+                                }
                         }
+                else if (child_pid > 0)
+                        wait(&status);
         }
         return (0);
 }
